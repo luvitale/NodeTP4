13cbdf0ffe9b73a7783dab547a02370318281a28
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2021-04-25 08:58:26 -0300

    Done delete button

--- public/js/list.js
+++ public/js/list.js
@@ -1,4 +1,16 @@
 setTimeout(
-  () => document.getElementById('product-state').style.display='none',
-  5000
+  () => {
+    let state = document.getElementById('product-state')
+    if (state) state.style.display='none'
+  }, 5000
 )
+
+$(document).ready(function () {
+  $('#deleteModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var productID = button.data('product-id') // Extract info from data-* attributes
+    var modal = $(this)
+    modal.find('.modal-body').text(`¿Estás seguro de eliminar el producto ${productID}?`)
+    document.getElementById('delete-form').action = `/borrar/${productID}?_method=DELETE`
+  })
+})
